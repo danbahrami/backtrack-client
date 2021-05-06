a9f85539bb3974cbdf025d14d3a1118ae5cf74f3
danbahrami <dan.bahrami@googlemail.com>
2021-05-06 15:00:04 +0100

    Fix Rollup warning about default exports

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,15 @@ export default {
   input: 'src/index.js',
   output: [
     {
-      dir: "dist/esm",
-      format: "esm",
-      exports: "named",
+      dir: 'dist/esm',
+      format: 'esm',
+      exports: 'auto',
       sourcemap: true,
     },
     {
-      dir: "dist/cjs",
-      format: "cjs",
-      exports: "named",
+      dir: 'dist/cjs',
+      format: 'cjs',
+      exports: 'auto',
       sourcemap: true,
     },
   ],
