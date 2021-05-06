febacd02029d087a5a90f2a5268d0239aeb2f1e3
danbahrami <dan.bahrami@googlemail.com>
2021-05-06 15:50:54 +0100

    Convert schema into JSON schema when defining events

--- src/define-event/define-event.js
+++ src/define-event/define-event.js
@@ -1,11 +1,40 @@
+function getSchemaRequiredFields(schemaTypes) {
+  return Object.keys(schemaTypes).reduce(
+    (acc, property) =>
+      schemaTypes[property]().required ? [...acc, property] : acc,
+    [],
+  );
+}
+
+function getSchemaProperties(schemaTypes) {
+  return Object.keys(schemaTypes).reduce(
+    (acc, property) => ({
+      ...acc,
+      [property]: schemaTypes[property]().definition,
+    }),
+    {},
+  );
+}
+
+function createJSONSchema(schemaTypes = {}) {
+  const required = getSchemaRequiredFields(schemaTypes);
+  const properties = getSchemaProperties(schemaTypes);
+
+  return {
+    type: 'object',
+    properties,
+    required,
+  };
+}
+
 export default function defineEvent(name, schema) {
   if (!name) {
     throw new Error('No name provided to defineEvent');
   }
 
-  return properties => ({
+  return (properties = {}) => ({
     name,
     properties,
-    schema,
+    schema: createJSONSchema(schema),
   });
 }
--- src/define-event/define-event.test.js
+++ src/define-event/define-event.test.js
@@ -2,10 +2,6 @@ import defineEvent from './define-event';
 import schemaTypes from '../schema-types';
 
 describe('defineEvent', () => {
-  it('throws an error when no name argument is provided', () => {
-    expect(() => defineEvent()).toThrow();
-  });
-
   it('returns an event factory function', () => {
     const buttonClicked = defineEvent('Button clicked');
 
@@ -13,8 +9,16 @@ describe('defineEvent', () => {
   });
 
   describe('the returned event factory function', () => {
-    const SCHEMA = { location: schemaTypes.string };
-    const PROPERTIES = { lodation: 'Masthead' };
+    const SCHEMA = {
+      location: schemaTypes.string,
+      userID: schemaTypes.number.isRequired,
+      isUserInTrial: schemaTypes.bool.isRequired,
+    };
+    const PROPERTIES = {
+      location: 'Masthead',
+      userID: 44,
+      isUserInTrial: false,
+    };
 
     it('returns an event object that includes the event name, schema and event properties', () => {
       const buttonClicked = defineEvent('Button clicked', SCHEMA);
@@ -23,7 +27,57 @@ describe('defineEvent', () => {
       expect(event).toEqual({
         name: 'Button clicked',
         properties: PROPERTIES,
-        schema: SCHEMA,
+        schema: expect.any(Object),
+      });
+    });
+
+    it('converts the schemaTypes definition into a JSON Schema format', () => {
+      const buttonClicked = defineEvent('Button clicked', SCHEMA);
+      const event = buttonClicked(PROPERTIES);
+
+      expect(event.schema).toEqual({
+        type: 'object',
+        required: ['userID', 'isUserInTrial'],
+        properties: {
+          location: { type: 'string' },
+          userID: { type: 'number' },
+          isUserInTrial: { type: 'boolean' },
+        },
+      });
+    });
+  });
+
+  describe('when an event is defined with no name', () => {
+    it('throws an error', () => {
+      expect(() => defineEvent()).toThrow();
+    });
+  });
+
+  describe('when an event is defined with no schema', () => {
+    it('creates an event with an empty JSON schema', () => {
+      const buttonClicked = defineEvent('Button clicked');
+      const event = buttonClicked();
+
+      expect(event.schema).toEqual({
+        type: 'object',
+        required: [],
+        properties: {},
+      });
+    });
+  });
+
+  describe('when the event factory is called with no properties', () => {
+    it('creates an event with an empty properties object', () => {
+      const buttonClicked = defineEvent('Button clicked', {
+        location: schemaTypes.string,
+      });
+
+      const event = buttonClicked();
+
+      expect(event).toEqual({
+        name: 'Button clicked',
+        properties: {},
+        schema: expect.any(Object),
       });
     });
   });
